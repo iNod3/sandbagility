32ddd187793677aba1b02b863e60a638d50b07bb
iNod3 <iNod3@nomail>
2018-06-13 00:19:59 +0200

    remove __init__.py

--- Sandbagility/Plugins/__init__.py
+++
@@ -1,7 +0,0 @@
-import pkgutil
-import inspect
-
-for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    module = loader.find_module(module_name).load_module(module_name)
-    for name, object in inspect.getmembers(module, inspect.isclass):
-        globals()[name] = object
